c711bb591510d407f8e9f067f3095c74a4840896
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2025-11-28 11:11:59 -0500

    Time: 7 ms (1.05%), Space: 17.9 MB (16.07%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,62 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        '''
+        input: integer num
+        output: integer result
+        goal: repeatedly add its digits
+            return when only 1 digit left
+
+        test case:
+            num = 38
+            output 2
+
+        constraint:
+            0<=num<=2^32-1
+
+        Questions:
+        min value of num? 0 no neg values
+        max value of num? 2^32-1
+
+
+        Approach:
+            3 + 8 = 11 < 10 (no)
+            1 + 1 = 2 < 10 (yes so stop)
+
+            0 < 10(yes) : stop
+
+            how to retrieve digits?
+                38 // 10 = 3
+                38 % 10 = 8
+                9 % 10 = 0 (stop)
+            iterate over added_num till remainder of added_num != 0:
+                while remainder(rem) != 0:
+                    digit = num // 10
+                    rem = num % 10
+                    add += digit`
+            return add_num
+        '''
+
+        res = num #2
+        while res >= 10: #false
+            print("res % 10", res%10)
+            initial_num = res #1
+            add_num = 0 #2
+            while (initial_num >= 10): #false
+                print("initial_num % 10", initial_num%10)
+                digit = initial_num // 10 #1
+                print("digit", digit)
+                add_num += digit
+                print("add_num", add_num)
+                initial_num = initial_num % 10 
+                print("initial_num", initial_num)
+                print("end of inner loop")
+            
+            add_num += initial_num  
+            res = add_num
+
+            print("add_num", add_num)
+            print("res", res)
+            print("end of outer looop")
+        return res
+
+        
