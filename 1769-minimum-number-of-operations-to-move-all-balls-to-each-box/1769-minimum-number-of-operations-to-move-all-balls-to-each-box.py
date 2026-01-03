37a8d7a7c36761f0fef1cc83cb2fb024bc3d87c6
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-01-02 21:39:24 -0500

    Time: 3289 ms (10.92%), Space: 17.7 MB (85.57%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,42 +1,53 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        
         '''
-        0   0   1   0   1   1    
-        0   0   0   1   2   4  operations left
-        0   0   0   1   1   2 balls left
-
-       11   8   5   3   1   0  operations right     
-       3    3   2   2   1   0    balls right
-
-
-
-        0   0   1   1   1   1    
-        0   0   0   1   3   6  operations
-        0   0   0   1   2   3 balls
-
-        operation[i] = balls[i-1] + operations[i-1] + (1 if boxes[i-1] == 1 else 0)
-        balls[i] = balls[i-1] + (1 if boxes[i-1] == 1 else 0)
-
-        '''
-        res = []
-        def get_operations(input_boxes):
-            balls, operations = [0], [0]
+        Input: string array boxes where element is either 0 or 1
+        Output: integer array where integer is number of operations
+        
+        '0':empty
+        1: 1 ball
+        Goal:
+            min operations needed to move all balls to ith box
+            
+        constraints:
+            1 operation = transfer 1 ball from box to adjacent box
+            
+        Questions:
+        in what direction do we move ball left or right? move in both direction
         
 
-            for i in range(1, len(input_boxes)):
-                total_balls = balls[i-1] + (1 if input_boxes[i-1] == '1' else 0)
-                balls.append(total_balls)
-                total_operations = balls[i-1] + operations[i-1] + (1 if input_boxes[i-1] == '1' else 0)
-                operations.append(total_operations)
-
-            return operations
-
-        left_operations = get_operations(boxes)
-        right_operations = get_operations(boxes[::-1])
-        right_operations.reverse()
-
-        for left, right in zip(left_operations, right_operations):
-            res.append(left+right)
-
+        property: number of operations for all balls to right and left side
+        
+        Approach:
+            iterate for each index
+                move to left 
+                move to right
+                in each direction, add right index - i = operations. 
+                add these operations if value is 1 else do not add
+            
+        
+        '''
+        res = [0] * len(boxes)
+        for i in range(len(boxes)):
+            left = i-1
+            count_left = 0
+            right = i + 1
+            count_right = 0
+            
+            while left > -1:
+                if boxes[left] == '1':
+                    count_left += (i - left)
+                left -= 1
+                
+            while right < len(boxes):
+                if boxes[right] == '1':
+                    count_right += (right-i) 
+                    
+                right += 1
+                
+            res[i] = count_right + count_left
+            
         return res
+                
         
