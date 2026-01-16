dd799e68991838478a7113049124adbd9d145cb8
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-01-16 17:33:44 -0500

    Time: 572 ms (74.62%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -31,7 +31,7 @@ SELECT
                 WHEN order_date = first_order AND order_date = customer_pref_delivery_date THEN 1
                 ELSE 0
             END
-        ) * 100 / COUNT(DISTINCT customer_id,
+        ) * 100 / COUNT(DISTINCT customer_id),
         2
-    )) AS immediate_percentage
+    ) AS immediate_percentage
 FROM cte;
