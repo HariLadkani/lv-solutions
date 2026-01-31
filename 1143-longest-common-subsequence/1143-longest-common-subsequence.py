06055ed307b41d0f1665d16c40ae186e67afeefa
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-01-30 19:46:20 -0500

    Time: 341 ms (88.3%), Space: 45.3 MB (27.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,65 +1,38 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         '''
-        min and max length of text1 and text2? 1 to 1000
-        does it contain only strs? yes
+        'abcde'
+            i
+        'ace'
+          j
 
-        output can be 0?  yes
-        max output: min(text1 len, text2 len)
+            res  112...3
 
-        brute force: generate sequences and find their match in another text2
-        run time = 2^n * m
+            observations:
+                if char equal: 
+                        current = dp[index-1][j-1] + 1
+                if char not equal:
+                        move first word
+                        move second word
+                        move both
 
-        Optimized:
-            dynamic programing
-            if characters match: 
-                    return (add 1 and find for rest of string longest common)
-            else:
-                return max(skip text1 or skip text2 index)
-
-            cache[(index1, index2)] = longest 
-
-                cache  = {}
-        def dfs(index1, index2):
-            if index1 == len(text1) or index2 == len(text2):
-                return 0
-
-            if (index1, index2) in cache:
-                return cache[(index1, index2)]
-                
-            length = 0
-            if text1[index1] == text2[index2]:
-                length += 1 + dfs(index1+1, index2+1)
-
-            else:
-                length = max(
-                    dfs(index1, index2+1),#move in text2
-                    dfs(index1+1, index2)) #move in text1
-            cache[(index1, index2)] = length
-            return length
-
-        return dfs(0, 0)
-
-        a b c d e -
-    a   3 2 2 1 1 
-    c   2 2 2 1 1
-    e   1 1 1 1 1
-    -    
-
+                a b c d e
+            a   1 1 1 1 1
+            c   1 1 2 2 2
+            e   1 1 2 2 3         
         '''
-        dp = [[0 for _ in range(len(text1)+1)] for _ in range(len(text2)+1)]
+        t1 = len(text1)
+        t2 = len(text2)
 
-        print(dp)
+        dp = [[0 for _ in range(t1+1)] for _ in range(t2+1)]
 
-        for index2 in range(len(text2)-1, -1, -1): #right to left
-            for index1 in range(len(text1)-1, -1, -1): #right to left
-                if text1[index1] == text2[index2]:
-                    dp[index2][index1] = 1 + dp[index2+1][index1+1]
-                
+        for i in range(t2):
+            for j in range(t1):
+                if text1[j] == text2[i]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[index2][index1] = max(dp[index2][index1+1], dp[index2+1][index1])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-        return dp[0][0]
+        return dp[t2-1][t1-1]
 
-
-
+        
