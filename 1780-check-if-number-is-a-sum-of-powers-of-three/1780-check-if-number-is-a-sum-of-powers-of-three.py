edc3188ea1afe7e4999016006e716909ecb529be
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-03-21 20:58:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,65 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        91/3 = 30 and rem=1 
+        
+        1, 3, 9, 27, 81 = 91
+
+
+        12 - 3 = 9
+        9 -3 = 6
+        6 - 3 = 3
+        3 -3 = 0  
+        4  
+
+        
+        21 - 3 = 18
+        18 - 3 = 15
+        15 - 3 = 12
+
+        4 + 3 = 7
+
+        91 - 3 = 87
+        ......
+
+        1 = 
+
+        30
+
+        21/3 = 7
+        6/3 = 2
+        2/3
+
+        12/3 = 4
+        3/3 = 0
+
+        90
+        90/3 = 30
+        30/3 = 10
+
+
+        1
+        
+
+        '''
+        res = False
+
+        arr = [3**i for i in range(int(n**(1/3))+1)]
+
+        def dfs(index, curr_sum):
+            nonlocal res
+            if index == len(arr):
+                if curr_sum == n:
+                    res = True
+
+                return
+
+            if curr_sum > n or res==True:
+                return False
+            #skip
+            dfs(index+1, curr_sum)
+            #take it
+            dfs(index+1, curr_sum + arr[index])
+        dfs(0, 0)
+        return res
+        
