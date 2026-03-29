10ca6a0e648eb56dd50d8c3a7dfd967fc493d246
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-03-29 14:03:52 -0400

    Time: 273 ms (31.61%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,9 +1,20 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+/*
+order by recordDate
 
-#unique columns:id, recordDate
-SELECT 
-w.id
-FROM Weather AS w
-INNER JOIN Weather AS w2
-ON DATEDIFF(w.recordDate, w2.recordDate) = 1
-WHERE w.temperature > w2.temperature;
+lag over temperature. if current temp > lag, return that as valid id
+if lag returns none, ignore that id
+
+
+*/
+WITH cte AS (
+SELECT
+    id,
+    LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp,
+    temperature,
+    LAG(recordDate) OVER (ORDER BY recordDate) AS prior_date,
+    recordDate
+FROM Weather
+)
+
+SELECT id FROM cte WHERE temperature > prev_temp and (recordDate - prior_date) = 1
