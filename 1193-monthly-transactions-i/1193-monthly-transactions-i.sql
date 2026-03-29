64b8028c171c063b672e16da53b75c245ce103fa
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-03-29 16:23:04 -0400

    Time: 443 ms (40.12%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,38 +1,16 @@
-# Write your MySQL query statement below
-/*
-Transactions
-    id(pk) | country | state | amount | trans_date
-
-
-goal:
-    for each month and country:
-        # of transactions
-        total amount
-        # of approved transactions
-        #total amount of approved transactions
-
-steps:
-    partition by month, country
-    sum(transactions)
-    use case statements to filter approved or non approved
-*/
-
-SELECT 
-    DATE_FORMAT(trans_date, '%Y-%m') AS month,
-    country,
+-- Write your PostgreSQL query statement below
+SELECT
+    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    MAX(country) AS country,
     COUNT(*) AS trans_count,
-    SUM(
-        CASE 
-            WHEN state = 'approved' THEN 1
-            ELSE 0
-        END
-    ) AS approved_count,
+    SUM(CASE 
+        WHEN state = 'approved' THEN 1 
+        ELSE 0
+    END) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(
-        CASE 
-            WHEN state = 'approved' THEN amount
-            ELSE 0
-        END
-    ) AS approved_total_amount
+    SUM(CASE 
+        WHEN state = 'approved' THEN amount
+        ELSE 0
+    END) AS approved_total_amount
 FROM Transactions
-GROUP BY MONTH(trans_date), YEAR(trans_date), country
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
