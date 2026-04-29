61a996131f595ab450d4066a48ef09a3f74b331e
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-04-28 23:15:59 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,85 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        '''
+
+        4 7  15 19
+        9 14 15 24
+        2 9  15 17
+
+        4  3    8   4 
+        13 8    9   13 
+        11 15   15  15
+        '''
+
+        def count_sum_in_rows(row_start, col_start):
+
+            #returns none if fails
+
+            sum_set = set()
+            value_set = set()
+            for row in range(row_start, row_start+3):
+                summation = 0
+                for col in range(col_start, col_start+3):
+                    summation += grid[row][col]
+                    value_set.add(grid[row][col])
+
+                sum_set.add(summation)
+                
+          
+            if len(sum_set) == 1 and len(value_set) == 9:
+                return sum_set.pop()
+
+        def count_sum_in_cols(row_start, col_start):
+            sum_set = set()
+            value_set = set()
+
+            for col in range(col_start, col_start+3):
+                summation = 0
+                for row in range(row_start, row_start+3):
+                    summation += grid[row][col]
+                    value_set.add(grid[row][col])
+                sum_set.add(summation)
+                
+
+            if (len(sum_set) == 1 and len(value_set) == 9):
+                return sum_set.pop()
+
+        def count_diagonals(row, col):
+            value_set_right = {
+                grid[row][col], grid[row+1][col+1], grid[row+2][col+2]
+            }
+            right_diag_sum = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2]
+   
+            left_diag_sum = grid[row][col+2] + grid[row+1][col+1] + grid[row+2][col]
+            value_set_left = {
+                grid[row][col+2], grid[row+1][col+1], grid[row+2][col]
+            }
+            
+            if len(value_set_right) == 3 and len(value_set_left) == 3 and right_diag_sum == left_diag_sum:
+                return right_diag_sum
+
+        result  = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        for row in range(0, ROWS-2):
+            for col in range(0, COLS-2):
+                sum_rows = count_sum_in_rows(row, col)
+                sum_cols = count_sum_in_cols(row, col)
+                sum_diag = count_diagonals(row, col)
+
+                if sum_rows and sum_cols and sum_diag and (sum_rows == sum_cols == sum_diag):
+                    result += 1
+
+        return result
+
+
+     
+        
+
+            
+        
+
+           
+
+
+
+
