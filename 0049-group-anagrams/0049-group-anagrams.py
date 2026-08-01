acae9b2713fadb5fafc16cab12cd219a3f0482ad
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-08-01 13:47:33 -0400

    Time: 51 ms (5.03%), Space: 27.1 MB (9.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,35 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        freq must match and so are chars for anagram
+
+        group by count using 26 letters of english
+
+        if len(strs) == 0:
+            return [[""]]
+        
+        loop over strs
+            loop over char and form a list with counts of each char where first elment in list stores counts of a and last stores counts of z
+        '''
+
+        if len(strs) == 0:
+            return [[""]]
+
+        res = []
+        anagram_map = defaultdict(list)
+
+        for string in strs:
+            freq_map = [0] * 26
+            for char in string:
+                freq_map[ord(char) - ord("a")] += 1
+            print("freq_map", freq_map)
+            key = tuple(freq_map)
+            anagram_map[key].append(string)
+
+        print(anagram_map)
+        for value in anagram_map.values():
+            res.append(value)
+
+        return res
+
+            
