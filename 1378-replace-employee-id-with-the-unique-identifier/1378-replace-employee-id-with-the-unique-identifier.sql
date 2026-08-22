82d1bfe4f44f2178ceb6c80ec6aa9359367c13ed
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-08-22 13:59:34 -0400

    Time: 1862 ms (7.33%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,12 +1,14 @@
-# Write your MySQL query statement below
-# reutrn all names from Employees table LEFT JOIN other table on first table id = second table id
+/*
+goal:
+    unique_id and namechoose 
 
+left join on id in employees and employeeuni table
+*/
 SELECT 
-    unique_id,
-    name 
-FROM 
-    Employees AS e
-LEFT JOIN 
-    EmployeeUNI AS u
-ON e.id = u.id;
+    u.unique_id,
+    e.name
+FROM Employees AS e
+LEFT JOIN EmployeeUNI AS u
+ON e.id=u.id;
+
 
