a2a777c54521df70533fbb74c57e2a5ea40a1960
Hari Lal <106039634+HariLadkani@users.noreply.github.com>
2026-08-22 13:14:31 -0400

    Time: 473 ms (85.35%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
-#not refered by: null
-#return names only
+/*
+output: return name where
+id not equal to 2 or id is null
+*/
 SELECT name
 FROM Customer
-WHERE referee_id != 2 or referee_id IS NULL;
+WHERE referee_id  != 2 OR referee_id  is NULL;
